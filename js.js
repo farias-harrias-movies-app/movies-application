500f7017353081fef7c3d86b66a269a51e7fe661
Daniel <daniel.h.farias2@gmail.com>
2023-04-11 15:21:24 -0500

    updated

--- js.js
+++ js.js
@@ -1,22 +1,42 @@
 //make a get request for the json server;
+let html = '';
 (function renderMovies() {
     $.ajax('http://localhost:3000/movies').done(data => {
         console.log(data)
-        let html = '<div class="card">'
-            // '<img>' + possible image
-        html += '<div class="card-body">'
-        html += `<h5>${data[0].title}</h5>`
-            //possible description
-        html += `<p>${data[0].rating}</p>`
-        html += '</div>'
-        html += '</div>'
-        $('#container').html(html)
+
+        data.forEach(data => {
+            console.log(data)
+            html += '<div class="card">'
+                // '<img>' + possible image
+            html += '<div class="card-body">'
+            html += `<h5>${data.title}</h5>`
+                //possible description
+            html += `<p>${data.rating}</p>`
+            html += '</div>'
+        })
+        html += '<form>'
+        html += '<label for="movie-title">'
+        html += '<input type="text" id="movie-title" name="movie-title">'
+        html += '<input type="text" id="movie-rating" name="movie-rating">'
+        html += '<button type="button">Submit</button>'
+        return $('#container').html(html)
     })
 })();
 
 //create boxes to display movie using json object
 
 //create a form for adding new objects to the json server
+function newMovie(){
+
+    $.ajax("http://localhost:3000/movies", {
+        type: "POST",
+        data: {
+            title: $('#moveie-title').val(),
+            ratings: $('#movie-rating').val()
+        }
+    });
+}
+$('button').click(newMovie());
 //attach the get after sending any alterations to the server
 //add and edit option
 //delete option to remove it from the json server
