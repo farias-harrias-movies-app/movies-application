d27fe355f54ed761c6317833aaa57d6760e53351
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-04-11 13:23:22 -0500

    added the psuedocode

---
+++ js.js
@@ -0,0 +1,7 @@
+//make a get request for the json server
+//create boxes to display movie using json object
+//create a form for adding new objects to the json server
+//attach the get after sending any alterations to the server
+//add and edit option
+//delete option to remove it from the json server
+//bonus use the omdb to render posters for the movie and possibly more information
