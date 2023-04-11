4ddd567c8f64d5d6973521ce1f858a3c84ed0a4b
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-04-11 14:18:23 -0500

    added the card structure for the movies

--- js.js
+++ js.js
@@ -1,5 +1,21 @@
-//make a get request for the json server
+//make a get request for the json server;
+(function renderMovies() {
+    $.ajax('http://localhost:3000/movies').done(data => {
+        console.log(data)
+        let html = '<div class="card">'
+            // '<img>' + possible image
+        html += '<div class="card-body">'
+        html += `<h5>${data[0].title}</h5>`
+            //possible description
+        html += `<p>${data[0].rating}</p>`
+        html += '</div>'
+        html += '</div>'
+        $('#container').html(html)
+    })
+})();
+
 //create boxes to display movie using json object
+
 //create a form for adding new objects to the json server
 //attach the get after sending any alterations to the server
 //add and edit option
